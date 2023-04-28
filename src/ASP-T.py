8d01e5d3b8409b7026abcef44205b95671b8d1d0
Tyrese <112655104+NotRichAnymore@users.noreply.github.com>
2023-04-28 20:53:02 +0100

    Main function (named after program)

---
+++ src/ASP-T.py
@@ -0,0 +1,11 @@
+from src.Controllers import mainController
+import pysnooper as ps
+
+
+def main():
+    main_function = mainController.MainController()
+    main_function.run()
+
+
+if __name__ == '__main__':
+    main()
