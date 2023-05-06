c75f319a075ef8c029767a78c73f1e9c8e90aaae
Tyrese <112655104+NotRichAnymore@users.noreply.github.com>
2023-05-06 19:01:35 +0100

    added help table

--- src/components/Repository/commandRepository.py
+++ src/components/Repository/commandRepository.py
@@ -13,7 +13,7 @@ class CommandRepository:
         return self.command.create_command_object(command_name, command_arguments, command_options, command_type)
 
     def create_initial_tables(self):
-        table_list = self.command_table(), self.argument_table(), self.options_table()
+        table_list = self.command_table(), self.arguments_table(), self.options_table()
         self.initial_sql.create_initial_tables(self.database_path, table_list)
 
     def get_by_path(self, path):
@@ -38,7 +38,8 @@ class CommandRepository:
     def command_table():
         command_table = """
         CREATE TABLE IF NOT EXISTS Commands (
-        command_name TEXT PRIMARY KEY,
+        id INTEGER AUTOINCREMENT PRIMARY KEY,
+        command_name TEXT,
         command_arguments TEXT,
         command_options TEXT,
         command_type TEXT)
@@ -49,9 +50,9 @@ class CommandRepository:
     def arguments_table():
         arguments_table = """
         CREATE TABLE IF NOT EXISTS Arguments (
-        command TEXT
+        command_id INTEGER AUTOINCREMENT,
         command_arguments TEXT),
-        FOREIGN KEY (command) REFERENCES Commands(command_name)
+        FOREIGN KEY (command_id) REFERENCES Commands(id)
         """
         return arguments_table
 
@@ -59,9 +60,23 @@ class CommandRepository:
     def options_table():
         options_table = """
         CREATE TABLE IF NOT EXISTS Options (
-        command TEXT
+        command_id INTEGER AUTOINCREMENT,
         command_options TEXT),
-        FOREIGN KEY (command) REFERENCES Commands(command_name)
+        FOREIGN KEY (command_id) REFERENCES Commands(id)
         """
         return options_table
+
+    @staticmethod
+    def help_command_table():
+        help_table = """
+        CREATE TABLE IF NOT EXISTS Commands (
+        id INTEGER AUTOINCREMENT,
+        command_info TEXT,
+        arguments_info TEXT,
+        options_info TEXT,
+        type_info TEXT),
+        FOREIGN KEY (id) REFERENCES Commands(id)
+        """
+        return help_table
+
 
