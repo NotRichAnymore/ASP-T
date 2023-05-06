165e3b0d8c2299030c95f7875e237293dbd73405
Tyrese <112655104+NotRichAnymore@users.noreply.github.com>
2023-05-06 17:25:41 +0100

    changed to fit MVC naming scheme

--- src/components/Controllers/windowController.py
+++ src/components/Controllers/windowController.py
@@ -1,11 +1,11 @@
-class WindowManager:
+class WindowController:
     def __init__(self):
         self.active_window_manager = {'previous_window': '', 'active_window': '', 'gui_status': None}
 
     def get_active_window_manager(self):
         return self.active_window_manager
 
-    def update_window_manager(self, previous, active, status):
+    def update_active_window_(self, previous, active, status):
         self.active_window_manager = {'previous_window': previous, 'active_window': active, 'gui_status': status}
 
     def get_previous_window(self):
