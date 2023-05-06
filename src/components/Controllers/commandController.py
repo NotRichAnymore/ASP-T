20cf56c54a5f979d108e065869eebea1b5faa5ad
Tyrese <112655104+NotRichAnymore@users.noreply.github.com>
2023-05-06 18:27:23 +0100

    moved date and sleep to validator

--- src/components/Controllers/commandController.py
+++ src/components/Controllers/commandController.py
@@ -9,26 +9,6 @@ class CommandController:
         self.service = service
         self.command = commands.Command(None)
 
-        
-
-    def valid_date_format(self, date_format):
-        try:
-            date.strftime(date.today(), date_format)
-            return True
-        except Exception as e:
-            print(e)
-            return False
-
-
-    def valid_sleep_format(self, time_format):
-        try:
-            time.strftime(time.localtime(), time_format)
-            return True
-        except Exception as e:
-            print(e)
-            return False
-
-
     def load_command(self, command_arguments):
         return self.service.parse(command_arguments)
 
