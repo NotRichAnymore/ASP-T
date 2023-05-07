bb197b84681d5935d7ef458fb7fbcff25284b4db
Tyrese <112655104+NotRichAnymore@users.noreply.github.com>
2023-05-07 03:35:17 +0100

    corrected build order

--- src/app/ASP-T.py
+++ src/app/ASP-T.py
@@ -1,13 +1,18 @@
 from src.components.Views.console import Console
+from src.components.Repository.commandRepository import CommandRepository
+from src.components.Validator.commandValidator import CommandValidator
+from src.components.Services.commandService import CommandService
+from src.components.Controllers.commandController import CommandController
+from src.components.Controllers.windowController import WindowController
 
 
 def build_program():
-    # repo = commandRepository()
-    # validator = commandValidator()
-    # service = commandService(repo, validator)
-    # controller = commandController(service)
-    # console = Console(controller)
-    console = Console()
+    repo = CommandRepository()
+    validator = CommandValidator()
+    service = CommandService(repo, validator)
+    command_controller = CommandController(service)
+    window_controller = WindowController()
+    console = Console(command_controller, window_controller)
     return console
 
 
