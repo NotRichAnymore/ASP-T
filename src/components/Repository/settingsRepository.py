fe3020133aa6dfa752545a89b79e64c7e3eecc62
Tyrese <112655104+NotRichAnymore@users.noreply.github.com>
2023-05-08 18:11:31 +0100

    initialise config method takes default path if one not specified

--- src/components/Repository/settingsRepository.py
+++ src/components/Repository/settingsRepository.py
@@ -1,6 +1,5 @@
 import configupdater
 import configparser
-import PySimpleGUI as sg
 from pathlib import Path
 from src.components.Utilities.utilities import create_error_message
 from src.components.Models.Settings import Settings
@@ -9,37 +8,37 @@ from src.components.Models.Settings import Settings
 class SettingsRepository:
 
     def __init__(self):
-        self.settings_obj = Settings()
+        self.model = Settings()
         self.updater = configupdater.ConfigUpdater()
         self.parser = configparser.ConfigParser()
         self.default_config_path = Path('src').resolve().parent.joinpath('data/files/config.ini').as_posix()
 
     def get_config_path(self):
-        return self.settings_obj.get_path()
+        return self.model.get_path()
 
     def set_config_path(self, path):
-        self.settings_obj.set_path(path)
+        self.model.set_path(path)
 
     def get_current_theme(self):
-        return self.settings_obj.get_theme()
+        return self.model.get_theme()
 
     def set_theme(self, theme):
-        self.settings_obj.set_theme(theme)
+        self.model.set_theme(theme)
 
     def set_save_folder(self, folder):
-        self.settings_obj.set_folder(folder)
+        self.model.set_folder(folder)
 
     def get_save_folder(self):
-        return self.settings_obj.get_folder()
+        return self.model.get_folder()
 
     def set_user_details_path(self, path):
-        self.settings_obj.set_user_details(path)
+        self.model.set_user_details(path)
 
     def get_user_details_path(self):
-        return self.settings_obj.get_user_details()
+        return self.model.get_user_details()
 
     def get_all_settings(self):
-        return self.settings_obj
+        return self.model
 
     def get_all_settings_by_section(self, section):
         pass
@@ -54,20 +53,20 @@ class SettingsRepository:
         return self.updater.to_dict()
 
     def save(self, settings):
-        with open(self.settings_obj.get_path(), 'w'):
+        with open(self.model.get_path(), 'w'):
             self.parser.write(settings)
 
     def config_exists(self):
-        return True if Path(self.settings_obj.get_path()).exists() else False
+        return True if Path(self.model.get_path()).exists() else False
 
     def read_config(self, config_path):
         try:
             with open(config_path, 'r') as config_file:
                 self.updater.read_file(config_file)
         except FileNotFoundError as e:
-            return create_error_message(error_type='settings',
-                                        message=f'Unable to read {config_path}',
-                                        error_message=str(e))
+            print(create_error_message(error_type='settings',
+                                       message=f'Unable to read {config_path}',
+                                       error_message=str(e)))
 
     def create_initial_settings(self):
         try:
@@ -76,7 +75,7 @@ class SettingsRepository:
                Theme = none
 
                [Files]
-               config.ini = {self.settings_obj.get_path()}
+               config.ini = {self.model.get_path()}
                save_folder = {Path(self.default_config_path).parent}
                user_details_path = none
                """
@@ -91,11 +90,13 @@ class SettingsRepository:
             print('Unable to write to config.ini')
 
     def initialise_config_file(self, config_path=None):
-        self.settings_obj.set_path(config_path)
-        if not self.config_exists():
-            self.settings_obj.set_path(self.default_config_path)
-            self.create_initial_settings()
-        self.read_config(self.get_config_path())
-        return sg.UserSettings(filename=config_path, use_config_file=True, convert_bools_and_none=True)
-
-
+        try:
+            self.model.set_path(self.default_config_path if config_path is None else config_path)
+            if not self.config_exists():
+                self.create_initial_settings()
+            self.read_config(self.get_config_path())
+            return self.model.create_settings_object()
+        except FileNotFoundError as e:
+            print(create_error_message(error_type='settings',
+                                       message=f'Unable to initialise settings path {config_path}',
+                                       error_message=str(e)))
