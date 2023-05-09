bd83dfbe2485d42f9fc4569e836b3cc0fcb2f005
Tyrese <112655104+NotRichAnymore@users.noreply.github.com>
2023-05-09 20:56:49 +0100

    programm running parmeter: default value supplied if not found

--- src/components/Controllers/windowController.py
+++ src/components/Controllers/windowController.py
@@ -5,8 +5,9 @@ class WindowController:
     def get_active_window_manager(self):
         return self.active_window_manager
 
-    def update_active_window_(self, previous, active, status):
-        self.active_window_manager = {'previous_window': previous, 'active_window': active, 'gui_status': status}
+    def update_active_window_(self, previous, active, status=None):
+        self.active_window_manager = {'previous_window': previous, 'active_window': active,
+                                      'gui_status': status if status is not None else True}
 
     def get_previous_window(self):
         return self.active_window_manager['gui_status']
