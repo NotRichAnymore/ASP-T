3a47ed2488dbb6103c26ac3df1090200697eb7b7
Tyrese <112655104+NotRichAnymore@users.noreply.github.com>
2023-05-28 04:19:23 +0100

    format, determining fields for command

--- src/components/command/Repository/commandRepository.py
+++ src/components/command/Repository/commandRepository.py
@@ -1,82 +1,38 @@
+import json
+import pysnooper
+from pathlib import Path
 from src.components.command.Models.Commands.commands import Command
 from src.components.Utilities.sqliteUtilities import SqliteUtilities
 
 
+@pysnooper.snoop()
 class CommandRepository:
 
     def __init__(self):
         self.command = Command(None)
         self.initial_sql = SqliteUtilities()
         self.database_path = None
+        self.command_details_path = Path('src').resolve().parent.parent.joinpath('data/files/command_details.json').as_posix()
 
     def create_command_object(self, command_name, command_arguments, command_options, command_type):
         return self.command.create_command_object(command_name, command_arguments, command_options, command_type)
 
-    def create_initial_tables(self):
-        table_list = self.command_table(), self.arguments_table(), self.options_table()
-        self.initial_sql.create_initial_tables(self.database_path, table_list)
+    def read_command_details(self):
+        with open(self.command_details_path, "r") as file:
+            jsonObj = json.load(file)
+            return jsonObj
 
-    def get_by_path(self, path):
-        pass
+    def get_all_commands(self):
+        commands = []
+        jsonObject = self.read_command_details()
+        for index in range(len(jsonObject)):
+            commands.append(jsonObject[index]['name'])
+        return commands
 
-    def get_command_arguments(self):
-        pass
-
-    def get_command_options(self):
-        pass
-
-    def get_command_type(self):
-        pass
-
-    def get_arguments_from_command_name(self, command_name):
-        pass
-
-    def get_options_from_command_name(self, command_name):
-        pass
-
-    @staticmethod
-    def command_table():
-        command_table = """
-        CREATE TABLE IF NOT EXISTS Commands (
-        id INTEGER AUTOINCREMENT PRIMARY KEY,
-        command_name TEXT,
-        command_arguments TEXT,
-        command_options TEXT,
-        command_type TEXT)
-        """
-        return command_table
-
-    @staticmethod
-    def arguments_table():
-        arguments_table = """
-        CREATE TABLE IF NOT EXISTS Arguments (
-        command_id INTEGER AUTOINCREMENT,
-        command_arguments TEXT),
-        FOREIGN KEY (command_id) REFERENCES Commands(id)
-        """
-        return arguments_table
-
-    @staticmethod
-    def options_table():
-        options_table = """
-        CREATE TABLE IF NOT EXISTS Options (
-        command_id INTEGER AUTOINCREMENT,
-        command_options TEXT),
-        FOREIGN KEY (command_id) REFERENCES Commands(id)
-        """
-        return options_table
-
-    @staticmethod
-    def help_command_table():
-        help_table = """
-        CREATE TABLE IF NOT EXISTS Help (
-        id INTEGER AUTOINCREMENT,
-        command_info TEXT,
-        arguments_info TEXT,
-        options_info TEXT,
-        type_info TEXT),
-        FOREIGN KEY (id) REFERENCES Commands(id)
-        """
-        return help_table
+    def get_command_format(self, command):
+        jsonObject = self.read_command_details()
+        for index in range(len(jsonObject)):
+            if jsonObject[index]['name'] == command:
+                return jsonObject[index]['name'], jsonObject[index]['arguments'], jsonObject[index]['options']
 
 
--- src/components/command/Services/commandService.py
+++ src/components/command/Services/commandService.py
@@ -1,7 +1,9 @@
 from src.components.Utilities.utilities import create_error_message
 import string
+import pysnooper
 
 
+@pysnooper.snoop()
 class CommandService:
     def __init__(self, repo, validator):
         self.repository = repo
@@ -22,57 +24,33 @@ class CommandService:
     def add_command_option(self, option):
         self.command_options.append(option)
 
-    def parse_command(self, user_defined_command):
-        command_name = self.repository.get_command_name
-        command_type = self.repository.get_command_type(user_defined_command)
-        # print('Validating command')
-        if self.validator.validate_command(command_name, user_defined_command):
-            # print('Command valid')
-            return user_defined_command, command_type
+    def establish_command_format(self, command):
+        return self.repository.get_command_format(command)
 
-    def parse_command_arguments(self, user_defined_arguments):
-        command_arguments = self.repository.get_command_arguments(user_defined_arguments)
+    def determine_command_arguments(self, command, arg_format, tokens):
+        match command:
+            case 'help':
+                for token in tokens:
+                    if token in self.repository.get_all_commands():
+                        return token
 
-        if self.validator.validate_command_arguments(command_arguments, user_defined_arguments):
-            return command_arguments
+    @staticmethod
+    def determine_command_options(command, opt_format, tokens):
+        match command:
+            case 'help':
+                return None
 
-    def parse_command_options(self, user_defined_options):
-        command_options = self.repository.get_command_options(user_defined_options)
+    def determine_command(self, tokens, command_format):
+        command = command_format[0]
+        command_args = self.determine_command_arguments(command, command_format[1], tokens[1:])
+        command_opts = self.determine_command_options(command, command_format[2], tokens[1:])
 
-        if self.validator.validate_command_options(command_options, user_defined_options):
-            return command_options
+        return [command, command_args, command_opts]
 
-    def determine_argument_order(self, user_input):
-        split_string = user_input.split(' ')
-        self.command_name = split_string[0]
-        arguments = self.repository.get_arguments_from_command_name(self.command_name)
-        options = self.repository.get_options_from_command_name(self.command_name)
-        for arg, opt, string_element in zip(arguments, options, split_string):
-            if not split_string[string_element] == 0:
-                match string_element:
-                    case str(arg) if str(arg) != 'None':
-                        self.add_command_argument(string_element)
-                    case str(opt) if str(opt) != 'None':
-                        self.add_command_option(string_element)
-
-        return self.command_name, self.command_arguments, self.command_options
-
-    def parse(self, user_input):
-        user_defined_command, user_defined_arguments, user_defined_options = \
-            self.determine_argument_order(user_input)
-
-        command_name, command_type = self.parse_command(user_defined_command)
-        command_arguments = self.parse_command_arguments(user_defined_arguments)
-        command_options = self.parse_command_options(user_defined_options)
-
-        status, valid_parses, invalid_parses = self.validator.parse_successful()
-        if status and invalid_parses is None:
-            self.validator.reset()
-            return self.repository.create_command_object(command_name, command_arguments, command_options, command_type)
-        else:
-            return create_error_message(error_type='parse',
-                                        message='Unable to parse user input',
-                                        error_message=None)
+    def parse(self, command_statement):
+        tokens = command_statement.split(' ')
+        command_format = self.establish_command_format(tokens[0])
+        return self.determine_command(tokens, command_format)
 
     def run_command(self, command):
         pass
